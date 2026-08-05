157b5b8f5ece6d6cff69aed31bf27f3fd0b1810e
subigya-js <developer.subigya@gmail.com>
2026-08-05 12:20:59 +0545

    update server port to 3001 and add additional routes

--- app.ts
+++ app.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from "express";
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
+})
+
+app.get("/about", (req: Request, res: Response) => {
+    res.send("This is about page");
+})
+
+app.get("/page/:id", (req: Request, res: Response) => {
+    res.send(`This is page: ${req.params.id}`);
+})
+
+app.get("/html", (req: Request, res: Response) => {
+    res.write(`<h1>This is html page</h1>`);
+    res.send();
+})
+
+app.get("/json", (req: Request, res: Response) => {
+    res.status(200).json(
+        {
+            id: 1,
+            name: "Subigya"
+        }
+    )
 })
 
 app.listen(PORT, () => {
